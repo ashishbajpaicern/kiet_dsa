a174ae3a8a0886fa6f980cf7a2fce5ea406a97bb
ashishbajpaicern <105496246+ashishbajpaicern@users.noreply.github.com>
2023-05-04 10:09:49 +0530

    updates

--- fibRecur.c
+++ fibRecur.c
@@ -1,1 +1,25 @@
-//
+#include <stdio.h>
+// program to implement fibonacci series using tail recursion
+int fib(int n, int a, int b)
+{
+    if (n == 0)
+    {
+        return a;
+    }
+    if (n == 1)
+    {
+        return b;
+    }
+    return fib(n - 1, b, a + b);
+}
+void main()
+{
+    int n;
+    printf("Enter the number of elements to be in the series : ");
+    scanf("%d", &n);
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d, ", fib(i, 0, 1));
+    }
+}
